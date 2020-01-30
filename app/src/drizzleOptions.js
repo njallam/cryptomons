b9dc10c3340b7abec18f89163e70b2785e305ac8
Nicholas Allam <njallam@outlook.com>
2020-01-30 13:53:37 +0000

    Actually use Browser Web3

--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -1,12 +1,6 @@
-import Web3 from 'web3';
-
 import Cryptomons from './contracts/Cryptomons.json';
 
 const options = {
-  web3: {
-    block: false,
-    customProvider: new Web3('ws://localhost:8545')
-  },
   contracts: [Cryptomons],
   polls: {
     accounts: 1500
